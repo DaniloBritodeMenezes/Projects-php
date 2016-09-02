7a5eded8b92f28ce6e1c62c26b481f930fd42a2a
Danilo Brito de Menezes <liloflexa@hotmail.com>
2016-09-02 06:18:43 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,22 @@
+<?php 
+?>
+<html>
+<head>
+
+<meta charset="UTF-8 " />
+<title>Sistema de cadastro</title>
+</head>
+<body>
+<form name="signup" method="post" action="cadastrando.php">
+Nome: <input type="text" name="Nome" /><br><br>
+Sobrenome: <input type="text" name="Sobrenome" /><br><br>
+País: <input type="text" name="País" /><br><br>
+Estado: <input type="text" name="Estado" /><br><br>
+Cidade: <input type="text" name="cidade" /><br><br>
+Email: <input type="text" name="Email" /><br><br>
+Senha: <input type="password" name="Sanha" /><br><br>
+<input Type="submit" value="cadastrar" />
+
+</form>
+</body>
+</html>
